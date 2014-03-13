0beec4edb8f2c0cfa2eda5b26256b5fa5ca261d3
ainma007 <b-sword@outlook.com>
2014-03-13 20:21:15 +0200

    Abs

---
+++ XpremaWhats/Xaina/RibbonForm1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+namespace Xaina
+{
+    public partial class RibbonForm1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public RibbonForm1()
+        {
+            InitializeComponent();
+        }
+    }
+}

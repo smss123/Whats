61deb52cc540677985f576bed9a09307b5ad9d8c
smss123 <smsssamer@live.com>
2014-03-13 21:50:27 +0400

    aa

--- XpremaWhats/Xaina/Program.cs
+++ XpremaWhats/Xaina/Program.cs
@@ -15,7 +15,7 @@ namespace Xaina
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainFrm());
+           // Application.Run(new MainFrm());
         }
     }
 }

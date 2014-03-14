f03ea7c88e7264774ec64b4d8c3f968648ea371c
smss123 <smsssamer@live.com>
2014-03-14 18:43:40 +0400

    Compelete Struct f WhatsAppNumber Class

--- XpremaWhats/Xprema/Commands/WhatsAppNumberCommand.cs
+++ XpremaWhats/Xprema/Commands/WhatsAppNumberCommand.cs
@@ -12,7 +12,27 @@ namespace Xprema.Commands
 
       }
 
-      public bool AddPhone(PhoneNumber Phone, DateTime Date, Name name)
+      public bool AddPhone(PhoneNumber Phone, DateTime Date, Name name, bool status, string Contry)
+      {
+          throw new NotImplementedException();
+      }
+
+      public bool EditPhone(PhoneNumber Phone, DateTime Date, Name name, bool status, string Contry, int ID)
+      {
+          throw new NotImplementedException();
+      }
+
+      public bool DeletePhone(int ID)
+      {
+          throw new NotImplementedException();
+      }
+
+      public List<Data.Xdb.WhatsUpNumberRow> GetAll()
+      {
+          throw new NotImplementedException();
+      }
+
+      public Data.Xdb.WhatsUpNumberRow GetNumberByID(int ID)
       {
           throw new NotImplementedException();
       }

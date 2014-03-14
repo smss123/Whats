06ca6e7370aad5f29c5a619ab7f7802c7562d107
ainma007 <b-sword@outlook.com>
2014-03-14 20:00:34 +0200

    ceratAddSenderFrm

--- XpremaWhats/Xaina/SenderNumber/frmAddSenderNumber.Designer.cs
+++ XpremaWhats/Xaina/SenderNumber/frmAddSenderNumber.Designer.cs
@@ -28,11 +28,262 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.AddNUmberBtn = new DevExpress.XtraEditors.SimpleButton();
+            this.CountrycomboBoxEdit = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.NametextEdit = new DevExpress.XtraEditors.TextEdit();
+            this.PhoneNumbertextEdit = new DevExpress.XtraEditors.TextEdit();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.NAmelayoutControlItem = new DevExpress.XtraLayout.LayoutControlItem();
+            this.simpleSeparator1 = new DevExpress.XtraLayout.SimpleSeparator();
+            this.simpleSeparator2 = new DevExpress.XtraLayout.SimpleSeparator();
+            this.CountrylayoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.NumberOfmessagetextEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.NumberOfmassagelayoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.CountertextEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.CounterlayoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CountrycomboBoxEdit.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NametextEdit.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PhoneNumbertextEdit.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NAmelayoutControlItem)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.simpleSeparator1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.simpleSeparator2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CountrylayoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumberOfmessagetextEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumberOfmassagelayoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CountertextEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CounterlayoutControlItem4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.CountertextEdit2);
+            this.layoutControl1.Controls.Add(this.NumberOfmessagetextEdit1);
+            this.layoutControl1.Controls.Add(this.AddNUmberBtn);
+            this.layoutControl1.Controls.Add(this.CountrycomboBoxEdit);
+            this.layoutControl1.Controls.Add(this.NametextEdit);
+            this.layoutControl1.Controls.Add(this.PhoneNumbertextEdit);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.layoutControl1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(354, 285);
+            this.layoutControl1.TabIndex = 1;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // AddNUmberBtn
+            // 
+            this.AddNUmberBtn.Appearance.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddNUmberBtn.Appearance.Options.UseFont = true;
+            this.AddNUmberBtn.Location = new System.Drawing.Point(12, 216);
+            this.AddNUmberBtn.Name = "AddNUmberBtn";
+            this.AddNUmberBtn.Size = new System.Drawing.Size(330, 57);
+            this.AddNUmberBtn.StyleController = this.layoutControl1;
+            this.AddNUmberBtn.TabIndex = 9;
+            this.AddNUmberBtn.Text = "Add";
+            // 
+            // CountrycomboBoxEdit
+            // 
+            this.CountrycomboBoxEdit.Location = new System.Drawing.Point(12, 192);
+            this.CountrycomboBoxEdit.Name = "CountrycomboBoxEdit";
+            this.CountrycomboBoxEdit.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.CountrycomboBoxEdit.Size = new System.Drawing.Size(330, 20);
+            this.CountrycomboBoxEdit.StyleController = this.layoutControl1;
+            this.CountrycomboBoxEdit.TabIndex = 8;
+            // 
+            // NametextEdit
+            // 
+            this.NametextEdit.Location = new System.Drawing.Point(12, 72);
+            this.NametextEdit.Name = "NametextEdit";
+            this.NametextEdit.Size = new System.Drawing.Size(330, 20);
+            this.NametextEdit.StyleController = this.layoutControl1;
+            this.NametextEdit.TabIndex = 6;
+            // 
+            // PhoneNumbertextEdit
+            // 
+            this.PhoneNumbertextEdit.Location = new System.Drawing.Point(12, 28);
+            this.PhoneNumbertextEdit.Name = "PhoneNumbertextEdit";
+            this.PhoneNumbertextEdit.Size = new System.Drawing.Size(330, 20);
+            this.PhoneNumbertextEdit.StyleController = this.layoutControl1;
+            this.PhoneNumbertextEdit.TabIndex = 4;
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.CustomizationFormText = "layoutControlGroup1";
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.NAmelayoutControlItem,
+            this.simpleSeparator1,
+            this.simpleSeparator2,
+            this.CountrylayoutControlItem3,
+            this.layoutControlItem3,
+            this.NumberOfmassagelayoutControlItem2,
+            this.CounterlayoutControlItem4});
+            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup1.Name = "Root";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(354, 285);
+            this.layoutControlGroup1.Text = "Root";
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.PhoneNumbertextEdit;
+            this.layoutControlItem1.CustomizationFormText = "Phone Number:";
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(334, 40);
+            this.layoutControlItem1.Text = "Phone Number:";
+            this.layoutControlItem1.TextLocation = DevExpress.Utils.Locations.Top;
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(132, 13);
+            // 
+            // NAmelayoutControlItem
+            // 
+            this.NAmelayoutControlItem.Control = this.NametextEdit;
+            this.NAmelayoutControlItem.CustomizationFormText = "Nmer";
+            this.NAmelayoutControlItem.Location = new System.Drawing.Point(0, 44);
+            this.NAmelayoutControlItem.Name = "NAmelayoutControlItem";
+            this.NAmelayoutControlItem.Size = new System.Drawing.Size(334, 40);
+            this.NAmelayoutControlItem.Text = "Name:";
+            this.NAmelayoutControlItem.TextLocation = DevExpress.Utils.Locations.Top;
+            this.NAmelayoutControlItem.TextSize = new System.Drawing.Size(132, 13);
+            // 
+            // simpleSeparator1
+            // 
+            this.simpleSeparator1.AllowHotTrack = false;
+            this.simpleSeparator1.CustomizationFormText = "simpleSeparator1";
+            this.simpleSeparator1.Location = new System.Drawing.Point(0, 40);
+            this.simpleSeparator1.Name = "simpleSeparator1";
+            this.simpleSeparator1.Size = new System.Drawing.Size(334, 2);
+            this.simpleSeparator1.Text = "simpleSeparator1";
+            // 
+            // simpleSeparator2
+            // 
+            this.simpleSeparator2.AllowHotTrack = false;
+            this.simpleSeparator2.CustomizationFormText = "simpleSeparator2";
+            this.simpleSeparator2.Location = new System.Drawing.Point(0, 42);
+            this.simpleSeparator2.Name = "simpleSeparator2";
+            this.simpleSeparator2.Size = new System.Drawing.Size(334, 2);
+            this.simpleSeparator2.Text = "simpleSeparator2";
+            // 
+            // CountrylayoutControlItem3
+            // 
+            this.CountrylayoutControlItem3.Control = this.CountrycomboBoxEdit;
+            this.CountrylayoutControlItem3.CustomizationFormText = "Country";
+            this.CountrylayoutControlItem3.Location = new System.Drawing.Point(0, 164);
+            this.CountrylayoutControlItem3.Name = "CountrylayoutControlItem3";
+            this.CountrylayoutControlItem3.Size = new System.Drawing.Size(334, 40);
+            this.CountrylayoutControlItem3.Text = "Country:";
+            this.CountrylayoutControlItem3.TextLocation = DevExpress.Utils.Locations.Top;
+            this.CountrylayoutControlItem3.TextSize = new System.Drawing.Size(132, 13);
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.AddNUmberBtn;
+            this.layoutControlItem3.CustomizationFormText = "layoutControlItem3";
+            this.layoutControlItem3.Location = new System.Drawing.Point(0, 204);
+            this.layoutControlItem3.MinSize = new System.Drawing.Size(82, 26);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(334, 61);
+            this.layoutControlItem3.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.layoutControlItem3.Text = "layoutControlItem3";
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem3.TextToControlDistance = 0;
+            this.layoutControlItem3.TextVisible = false;
+            // 
+            // NumberOfmessagetextEdit1
+            // 
+            this.NumberOfmessagetextEdit1.Location = new System.Drawing.Point(12, 112);
+            this.NumberOfmessagetextEdit1.Name = "NumberOfmessagetextEdit1";
+            this.NumberOfmessagetextEdit1.Size = new System.Drawing.Size(330, 20);
+            this.NumberOfmessagetextEdit1.StyleController = this.layoutControl1;
+            this.NumberOfmessagetextEdit1.TabIndex = 10;
+            // 
+            // NumberOfmassagelayoutControlItem2
+            // 
+            this.NumberOfmassagelayoutControlItem2.Control = this.NumberOfmessagetextEdit1;
+            this.NumberOfmassagelayoutControlItem2.CustomizationFormText = "Number Of Message In Day";
+            this.NumberOfmassagelayoutControlItem2.Location = new System.Drawing.Point(0, 84);
+            this.NumberOfmassagelayoutControlItem2.Name = "NumberOfmassagelayoutControlItem2";
+            this.NumberOfmassagelayoutControlItem2.Size = new System.Drawing.Size(334, 40);
+            this.NumberOfmassagelayoutControlItem2.Text = "Number Of Message In Day";
+            this.NumberOfmassagelayoutControlItem2.TextLocation = DevExpress.Utils.Locations.Top;
+            this.NumberOfmassagelayoutControlItem2.TextSize = new System.Drawing.Size(132, 13);
+            // 
+            // CountertextEdit2
+            // 
+            this.CountertextEdit2.Location = new System.Drawing.Point(12, 152);
+            this.CountertextEdit2.Name = "CountertextEdit2";
+            this.CountertextEdit2.Size = new System.Drawing.Size(330, 20);
+            this.CountertextEdit2.StyleController = this.layoutControl1;
+            this.CountertextEdit2.TabIndex = 11;
+            // 
+            // CounterlayoutControlItem4
+            // 
+            this.CounterlayoutControlItem4.Control = this.CountertextEdit2;
+            this.CounterlayoutControlItem4.CustomizationFormText = "Counter";
+            this.CounterlayoutControlItem4.Location = new System.Drawing.Point(0, 124);
+            this.CounterlayoutControlItem4.Name = "CounterlayoutControlItem4";
+            this.CounterlayoutControlItem4.Size = new System.Drawing.Size(334, 40);
+            this.CounterlayoutControlItem4.Text = "Counter";
+            this.CounterlayoutControlItem4.TextLocation = DevExpress.Utils.Locations.Top;
+            this.CounterlayoutControlItem4.TextSize = new System.Drawing.Size(132, 13);
+            // 
+            // frmAddSenderNumber
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 277);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "frmAddSenderNumber";
             this.Text = "frmAddSenderNumber";
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.CountrycomboBoxEdit.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NametextEdit.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PhoneNumbertextEdit.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NAmelayoutControlItem)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.simpleSeparator1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.simpleSeparator2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CountrylayoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumberOfmessagetextEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumberOfmassagelayoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CountertextEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CounterlayoutControlItem4)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraEditors.SimpleButton AddNUmberBtn;
+        private DevExpress.XtraEditors.ComboBoxEdit CountrycomboBoxEdit;
+        private DevExpress.XtraEditors.TextEdit NametextEdit;
+        private DevExpress.XtraEditors.TextEdit PhoneNumbertextEdit;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.LayoutControlItem NAmelayoutControlItem;
+        private DevExpress.XtraLayout.SimpleSeparator simpleSeparator1;
+        private DevExpress.XtraLayout.SimpleSeparator simpleSeparator2;
+        private DevExpress.XtraLayout.LayoutControlItem CountrylayoutControlItem3;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private DevExpress.XtraEditors.TextEdit CountertextEdit2;
+        private DevExpress.XtraEditors.TextEdit NumberOfmessagetextEdit1;
+        private DevExpress.XtraLayout.LayoutControlItem NumberOfmassagelayoutControlItem2;
+        private DevExpress.XtraLayout.LayoutControlItem CounterlayoutControlItem4;
     }
 }

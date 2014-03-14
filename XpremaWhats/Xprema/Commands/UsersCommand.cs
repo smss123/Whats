f8acf450581ddaa3132b8b48daba06d806c40fde
smss123 <smsssamer@live.com>
2014-03-14 20:02:00 +0400

    Add User Command

---
+++ XpremaWhats/Xprema/Commands/UsersCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xprema.XdataType;
+
+namespace Xprema.Commands
+{
+  public   class UsersCommand
+    {
+
+      public bool NewUser(Name UserName, string Password)
+      {
+          throw new NotImplementedException();
+      }
+      public bool EditUser(Name UserName, string Password, int ID)
+      {
+          throw new NotImplementedException();
+      }
+
+      public bool DeleteUser(int ID)
+      {
+          throw new NotImplementedException();
+      }
+
+      public bool Login(Name UserName, string Password)
+      {
+          throw new NotImplementedException();
+      }
+
+
+      public List<Data.Xdb.UsersRow> GetAll()
+      {
+          throw new NotImplementedException();
+      }
+
+
+    }
+}

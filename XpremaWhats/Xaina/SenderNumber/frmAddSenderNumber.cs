97579b19ff080620467ca506856538099d44cdc9
smss123 <smsssamer@live.com>
2014-03-14 21:46:21 +0400

    Add Form

---
+++ XpremaWhats/Xaina/SenderNumber/frmAddSenderNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Xaina.SenderNumber
+{
+    public partial class frmAddSenderNumber : DevExpress.XtraEditors.XtraForm
+    {
+        public frmAddSenderNumber()
+        {
+            InitializeComponent();
+        }
+    }
+}

b9cac24a7c979acfd8545cf1577695f9be53bc11
smss123 <smsssamer@live.com>
2014-03-14 19:30:40 +0400

    Adding SenderQuee Class

---
+++ XpremaWhats/Xprema/Commands/SenderQueeCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xprema.Commands
+{
+public     class SenderQueeCommand
+    {
+
+    public bool AddSenderQuee(int AdvertiseID, string SerialNumberm, bool Status)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool EditSenderQuee(int AdvertiseID, string SerialNumberm, bool Status , int ID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeleteSenderQuee(int ID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Data.Xdb.SenderQueeRow> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Data.Xdb.SenderQueeRow GetByID(int ID)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    }
+}

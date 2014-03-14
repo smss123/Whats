9f0c9d7c531c2deca27574fc17a1918ab110e8bd
smss123 <smsssamer@live.com>
2014-03-14 19:51:56 +0400

    Compelete Struct Classes

---
+++ XpremaWhats/Xprema/Data/XpremaFileManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xprema.Data
+{
+public     class XpremaFileManager
+    {
+
+
+    // this Class To Mange Xml File
+
+
+    }
+}

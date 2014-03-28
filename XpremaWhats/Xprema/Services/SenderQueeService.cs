cb36153867437075de368d5838e3989bebe8ca39
smss123 <smsssamer@live.com>
2014-03-29 01:54:58 +0400

    Create SenderService

---
+++ XpremaWhats/Xprema/Services/SenderQueeService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xprema.Services
+{
+   public  class SenderQueeService   :IXpremaService
+    {
+
+        public void StartService()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopService()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Status
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public double CurrntStep
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
